4950aad4b3763c35efb8a4de2959b3b0bde40fca
liujohnson118 <liujohnson118@yahoo.com>
2017-07-11 17:48:43 +0000

    set up testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if User.where(email:user.email.downcase).count<1
-      if (user.save && user.password.length>6)
+      if (user.save)
         session[:user_id] = user.id
         redirect_to '/'
       else
